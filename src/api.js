615560828735fdf72a2981e36f74f5d85509edc8
Hernán Peralta <alegodemencia@gmail.com>
2020-04-27 17:10:29 -0300

    Update api.js
    Added class Rate

--- src/api.js
+++ src/api.js
@@ -1,8 +1,17 @@
 const BASE_URL = "https://api.exchangeratesapi.io"
 
+class Rates {
+  constructor(resJSON){
+    this.base = resJSON.base;
+    this.date = resJSON.date;
+    this.rates = resJSON.rates
+  }
+}
+
 export function getData(url = `${BASE_URL}/latest`) {
   return fetch(url)
     .then(res => res.json())
+    .then(resJSON => new Rates(resJSON))
     .catch(error => console.error("Failed", error));
 }
 
